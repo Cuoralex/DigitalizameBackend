1f86ebaa0d9ba9625f4985b34c2afa7b1baf74bd
Cuoralex <alexacuartas@gmail.com>
2025-08-06 13:37:02 -0500

    inicio de proyecto

---
+++ src/main/java/com/digitalizame/DigitalizameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.digitalizame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalizameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalizameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalizame/DigitalizameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalizame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalizameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
